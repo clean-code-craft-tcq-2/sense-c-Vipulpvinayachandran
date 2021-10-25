f5ef32813e337449cdf5f1a25218ca041e98dd82
Vipulpvinayachandran <91024742+Vipulpvinayachandran@users.noreply.github.com>
2021-10-25 13:44:55 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -10,11 +10,12 @@ struct stats
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
 
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats compute_statistics)
+void compute_statistics(const float* numberset, int setlength)
+{
+ return(emailAlertCallCount,ledAlertCallCount);
+}
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
 {
  return(s.max,s.average,
 }
-void compute_statistics(const float* numberset, int setlength);
-{
- 
-}
+
