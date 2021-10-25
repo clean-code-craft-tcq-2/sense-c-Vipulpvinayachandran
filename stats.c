1d0f2c85e3f5998f42a01e70f82f98d3c5662274
Vipulpvinayachandran <91024742+Vipulpvinayachandran@users.noreply.github.com>
2021-10-25 18:36:37 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,16 +1,44 @@
 #include "stats.h"
+#include "math.h"
+
+Stats compute_statistics(const float* numberset, int setlength) {
+    Stats s;
+    
+    if(setlength != 0)
+    { 
+        int i;
+        float sum;
+    
+         s.max = numberset[0];
+         s.min = numberset[0];
+         sum = numberset[0];
+        
+        for (i=1; i<setlength; i++)
+        {
+          if (numberset[i]> s.max)
+          {
+              s.max=numberset[i];
+          }
+          else if (numberset[i]<s.min)
+          {
+             s.min=numberset[i];
+          }
+          else
+          {
+              /* do nothing */
+          }    
+          sum += numberset[i];
+        }
+        s.average = sum/setlength;
+    }
+    else
+    {
+        s.max = NAN;
+        s.min = NAN;
+        s.average = NAN;
+    }
+    return s;
+}
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
-void compute_statistics(const float* numberset, int setlength)
-{
-  return(emailAlertCallCount,ledAlertCallCount);
-}
-float max =0;
-int counters[4] ={1,2,3,4};
-
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
-{
-  return(max,counters[],
-}
-
